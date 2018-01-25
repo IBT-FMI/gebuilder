163046fbb1c1f2d955a63479975c5934f9851d3a
Horea Christian <horea.christ@yandex.com>
2018-01-25 20:40:13 +0100

    Emerging required vcs

--- gebuilder/scripts/common/sync.sh
+++ gebuilder/scripts/common/sync.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+for VCS in "cvs" "git" "mercurial" "subversion"; do
+	echo "Checking if repos need ${VCS}."
+	grep -qr ${VCS} /etc/portage/repos.conf/ &&
+		echo "Apparently ${VCS} is needed, emerging..." &&
+		emerge dev-vcs/${VCS}
+done
+
 emaint sync -a
