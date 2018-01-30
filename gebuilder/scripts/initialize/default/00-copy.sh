7560a9e7f166edf2b1c74bf42bcaf987c19c5492
Doeme <das1993@hotmail.com>
2018-01-30 12:11:09 +0000

    Don't delete logs on failure to initialize
    This addresses part of issue #4

--- gebuilder/scripts/initialize/default/00-copy.sh
+++ gebuilder/scripts/initialize/default/00-copy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-if [[ -v DELETE_ON_FAIL ]]; then on_error "rm --one-file-system -r '${ROOT%root}'"; fi
+if [[ -v DELETE_ON_FAIL ]]; then on_error "rm --one-file-system -r '${ROOT}'"; fi
 
 rsync -a roots/stemgentoo/root/ "${ROOT}"
