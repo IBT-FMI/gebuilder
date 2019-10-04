9fee0a66d72258ad7aa79b12f7ac0fe4e5fcc23d
Blaise Morel <bmorel@student.ethz.ch>
2019-10-04 13:32:59 +0200

    Test wether stmegentoo has been initialized

--- gebuilder/scripts/initialize/default/00-copy.sh
+++ gebuilder/scripts/initialize/default/00-copy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ ! -f  "/var/lib/gebuilder/roots/stemgentoo/root/" ]; then 
+       error "Stemgen was not initialized"
+       error exit
+fi
+
 if [[ -v DELETE_ON_FAIL ]]; then on_error "rm --one-file-system -r '${ROOT}'"; fi
 
-rsync -a roots/stemgentoo/root/ "${ROOT}"
+rsync -a /var/lib/gebuilder/roots/stemgentoo/root/ "${ROOT}"
