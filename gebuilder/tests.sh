9a82600009e94146b286b8b20a074e9d2a6de99d
Dominik Schmidt <dominik@schm1dt.ch>
2019-10-04 11:22:34 +0000

    Cache error and ok-messages instead of immediately outputting them.
    This allows giving a feedback at the end.

--- gebuilder/tests.sh
+++ gebuilder/tests.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
 source "$(dirname "$0")/utils/functions.sh"
+
+msgs=()
+
+function error(){
+	msgs+=("ERROR: $*")
+}
+
+function ok(){
+	msgs+=("OK: $*")
+}
+
 trap - ERR
 
 if [ -z "${GEBUILDER_ENTRY+x}" ]
@@ -24,4 +35,9 @@ do
 	else
 		error "$TEST failed"
 	fi
+done
+
+for line in "${msgs[@]}"
+do
+	echo "$line"
 done
