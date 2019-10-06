a5a9ab8078eb0ac96889e16120c370a5f6102dc0
Dominik Schmidt <dominik@schm1dt.ch>
2019-10-06 11:04:38 +0000

    Fix the dotgentoo specific tests in testsuite

--- gebuilder/tests/30_test_dotgentoo_initialize.sh
+++ gebuilder/tests/30_test_dotgentoo_initialize.sh
@@ -6,13 +6,13 @@ DOTGENTOO="${GEBUILDER_ROOT}/tests/dotgentoos/.gentoo"
 
 DOTGENTOO_ID="$(get_dotgentoo_id "${DOTGENTOO}")"
 "$GEBUILDER_ENTRY" "${DOTGENTOO}" initialize
-if [ "$?" -eq 0 ]
+if [ ! "$?" -eq 0 ]
 then
 	error "Initialization of .gentoo failed"
 	RET=1
 fi
 
-if [ "roots/${DOTGENTOO_ID}/root/usr/include/gmp.h" ]
+if [ ! -f "roots/${DOTGENTOO_ID}/root/usr/include/gmp.h" ]
 then
 	error "GMP dependency not pulled in"
 	RET=1
