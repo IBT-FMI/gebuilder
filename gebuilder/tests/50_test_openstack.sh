002e625997e99a7dba2411fe99c1b8cff4b09eb1
Dominik Schmidt <dominik@schm1dt.ch>
2019-10-20 15:56:36 +0000

    WIP of advanced openstack image testing using qemu
    This is still defunct and untested. The problem is that the openstack
    image closes the serial port after "* Starting Local"

--- gebuilder/tests/50_test_openstack.sh
+++ gebuilder/tests/50_test_openstack.sh
@@ -7,4 +7,42 @@ if [ ! "$?" -eq 0 ]
 then
 	error "Generating openstack image for a .gentoo failed"
 	RET=1
+else
+	CLOUD_INIT=0
+	LOGIN=0
+	(
+	qemu-system-x86_64 -hda "roots/${DOTGENTOO_ID}/openstack_images/"* -serial stdio -display none -nographic -snapshot -m 512 | while read line
+	do
+		case "$line" in
+			"Cloud-init"*)
+				CLOUD_INIT=1
+			;;
+			"login:"*)
+				LOGIN=1
+			*)
+			;;
+		esac
+	done
+	)&
+	PID=$?
+	sleep $((60*10))&
+	wait -n
+	if [ -e "/proc/${PID}" ]
+	then
+		error "The VM timed out"
+		kill -s KILL "${PID}"
+		RET=1
+	fi
+
+	if [ "${CLOUD_INIT}" -eq 0 ]
+	then
+		error "Cloud Init was not called"
+		RET=1
+	fi
+
+	if [ "${LOGIN}" -eq 0 ]
+	then
+		error "No login screen reached"
+		RET=1
+	fi
 fi
