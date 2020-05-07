035e93fd1896a8cb9044851719d02f4d3702cd79
Horea Christian <chr@chymera.eu>
2020-05-07 17:26:03 -0400

    Using package name for image

--- gebuilder/scripts/stage4_image/default/10-make_tarball.sh
+++ gebuilder/scripts/stage4_image/default/10-make_tarball.sh
@@ -6,7 +6,6 @@ STAGE4_IMAGE_NAME=${STAGE4_IMAGE_NAME##*/}
 
 STAGE4_IMAGE_DIR="${ROOT}/../stage4_images/"
 ensure_dir "${STAGE4_IMAGE_DIR}"
-STAGE4_IMAGE_NAME="stage4"
 STAGE4_IMAGE="${STAGE4_IMAGE_DIR}/${STAGE4_IMAGE_NAME}"
 
 debug "Creating tarball..."
